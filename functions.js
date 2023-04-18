a62a922174409e6e18ff3517f13693798ce05652
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-04-18 08:59:56 +0300

    commit

---
+++ functions.js
@@ -0,0 +1,50 @@
+//You manage a grocery store and need to keep track of the inventory of 
+//various items. You will use arrays to store the data and various
+// functions to manipulate and analyze the data.
+
+//Create an array containing the names of all items in the inventory 
+//maximum of 10.
+let arrayitems=["zucchini","tomatoes","mushrooms","cucumbers","apples","eggs",
+"bananas","celery","lettuce","poultry"]
+
+//Create a separate array with the corresponding stock quantities of each
+// item maximum of 10.
+let arrquantities=[50,250,290,56,45,90,120,33,15,36]
+
+//Write a function to add a new item to the inventory,
+// updating both arrays.
+function newitems(arrayitems,arrquantities){ 
+arrayitems.push("beef")
+
+arrquantities.push(60)
+
+console.log(arrayitems,arrquantities)
+}
+newitems(arrayitems,arrquantities)
+//Write a function to update the stock quantity of an existing item.
+function update(){
+for(let i=0;i < arrquantities.length;i++)
+if(newitems== arrquantities[i].update)
+arrquantities;quantity++
+
+
+}
+
+
+//Write a function to calculate the total number of items in the
+// inventory.
+function totalItems(){
+    arrayitems.push("milk")
+    console.log(arrayitems)
+
+    arrquantities.push(20)
+    console.log(arrquantities)
+
+}
+totalItems(arrayitems,arrquantities)
+//Write a function to find the item with the lowest stock quantity. 
+function loWQantities(){
+
+} 
+
+
